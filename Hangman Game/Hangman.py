586e1881d5c8dd32280dd3959974041cba0610b7
Ali Kashani <alikashani9775@gmail.com>
2025-04-24 19:34:41 -0400

    Adding appendix file to Hangman

--- Hangman Game/Hangman.py
+++ Hangman Game/Hangman.py
@@ -1,7 +1,8 @@
-import Hangman_appendix
+from Hangman_appendix import *
+
+lives = 6
 
 chosen_word = random.choice(words_list)
-print(chosen_word)
 
 placeholder = ""
 for i in range(0,len(chosen_word)):
@@ -9,24 +10,32 @@ for i in range(0,len(chosen_word)):
 
 print(placeholder)
 
+game = False
 correct_letters = []
 
-game = False
-
 while game is not True:
+    print(f"You have got {lives} lives left!")
     display = ""
     guess = input("Guess a letter: ").lower()
+    if guess in correct_letters:
+        print(f"You have already guessed {guess} before!")
     for letter in chosen_word:
-        if letter == guess:
-            display = display + letter
-            correct_letters.append(letter)
-        elif letter in correct_letters:
-            display = display + letter
-        else:
-            display = display + "_"
-        
-        if display == chosen_word:
-            game = True
-            print("You Won!")
-
+      if letter == guess:
+        display += letter
+        correct_letters.append(letter)
+      elif letter in correct_letters:
+          display += letter
+      else:
+          display = display + "_"
     print(display)
+        
+    if display == chosen_word:
+        game = True
+        print("*******  You Won!  *******")
+    if guess not in chosen_word:
+        lives -= 1
+        if lives == 0:
+            game = True
+            print("*******  You Lost!  *******")
+
+    print(stages[lives])
--- Hangman Game/Hangman_appendix.py
+++ Hangman Game/Hangman_appendix.py
@@ -58,4 +58,15 @@ stages = ['''
 =========
 ''']
 
-words_list = ["aardvark", "baboon", "camel"]
+words_list = [
+    "apple", "banana", "camera", "dolphin", "elephant", "forest", "galaxy", "hammer", "igloo", "jacket",
+    "kingdom", "lemonade", "monster", "notebook", "octopus", "pirate", "quilt", "rainbow", "spaceship", "treasure",
+    "universe", "volcano", "wizard", "xylophone", "yoga", "zebra", "anchor", "bicycle", "castle", "dragon",
+    "eagle", "firefly", "garden", "horizon", "island", "jellyfish", "koala", "lantern", "magic", "necklace",
+    "ocean", "puzzle", "rocket", "snowflake", "thunder", "umbrella", "vampire", "waterfall", "xenon", "yarn",
+    "zipper", "acrobat", "blizzard", "canyon", "dentist", "emerald", "festival", "giraffe", "hurricane", "iceberg",
+    "jungle", "kangaroo", "lighthouse", "meteor", "ninja", "oasis", "parrot", "quartz", "rescue", "safari",
+    "tornado", "unicorn", "vulture", "whirlpool", "adventure", "blueprint", "crocodile", "dimension", "energy", "fortress",
+    "goblin", "hacker", "illusion", "justice", "knowledge", "labyrinth", "mechanic", "nightmare", "orbit", "pharaoh",
+    "quest", "robot", "satellite", "timezone", "universe", "villain", "warrior", "xenophobia", "yawning", "zeppelin"
+]
